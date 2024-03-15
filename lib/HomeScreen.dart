b9e2577460e4497d37f92de3c1371a045b88f261
sanyam <sanyamnarang40@gmail.com>
2024-03-15 10:07:57 +0530

    Refactor HindiGenre widget and add search functionality and rating filters

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,188 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:moviecatalog/genre-Hindi.dart';
+import 'package:moviecatalog/menu-drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF00050D),
+      appBar: AppBar(
+        title: const Text(
+          'MovieCatalog App',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromRGBO(12, 41, 65, 1),
+      ),
+      drawer: const MenuDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Genre',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'My Account',
+          ),
+        ],
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.black,
+        backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HindiGenre(),
+                      ),
+                    );
+                  },
+                  child: Image.network(
+                    'https://res.cloudinary.com/dtvaexasg/image/upload/v1710393330/flutter/movie_1.jpg',
+                    height: 400,
+                    width: 400,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HindiGenre(),
+                      ),
+                    );
+                  },
+                  child: Image.network(
+                    'https://res.cloudinary.com/dtvaexasg/image/upload/v1710393546/flutter/movie_2.jpg',
+                    height: 400,
+                    width: 400,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+              options: CarouselOptions(
+                height: 300,
+                viewportFraction: 1.0,
+                autoPlay: true,
+                enlargeCenterPage: false,
+              ),
+            ),
+            SizedBox(
+              height: 400,
+              child: GridView.count(
+                crossAxisCount: 2,
+                scrollDirection: Axis.vertical,
+                children: [
+                  _buildCategoryItem('Action',
+                      'https://res.cloudinary.com/dtvaexasg/image/upload/v1710396367/flutter/hindi.png',
+                      () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HindiGenre(),
+                      ),
+                    );
+                  }),
+                  _buildCategoryItem('Comedy',
+                      'https://res.cloudinary.com/dtvaexasg/image/upload/v1710397585/flutter/english.png',
+                      () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HindiGenre(),
+                      ),
+                    );
+                  }),
+                  _buildCategoryItem('Drama',
+                      'https://res.cloudinary.com/dtvaexasg/image/upload/v1710397708/flutter/punjabi.png',
+                      () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HindiGenre(),
+                      ),
+                    );
+                  }),
+                  _buildCategoryItem('Thriller',
+                      'https://res.cloudinary.com/dtvaexasg/image/upload/v1710397709/flutter/gujrati.png',
+                      () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HindiGenre(),
+                      ),
+                    );
+                  }),
+                  _buildCategoryItem('Horror',
+                      'https://res.cloudinary.com/dtvaexasg/image/upload/v1710397711/flutter/telugu.png',
+                      () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const HindiGenre(),
+                      ),
+                    );
+                  }),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20),
+                  SizedBox(height: 20),
+                  SizedBox(height: 2),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryItem(
+      String category, String imageUrl, Function()? onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: const Color(0xFF00050D),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Image.network(
+          imageUrl,
+          height:
+              double.infinity, // Set height to fill available space vertically
+          width:
+              double.infinity, // Set width to fill available space horizontally
+          fit: BoxFit.cover, // Use cover to fill the entire space
+        ),
+      ),
+    );
+  }
+}
--- lib/genre-Hindi.dart
+++ lib/genre-Hindi.dart
@@ -1,19 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:moviecatalog/menu-drawer.dart';
 
 class HindiGenre extends StatelessWidget {
   const HindiGenre({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF00050D),
-      body: Center(
-        child: Text(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
           'Hindi Genre',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 96, 3, 3),
+      ),
+      drawer: const MenuDrawer(),
+      backgroundColor: const Color(0xFF00050D),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Search by name',
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
           ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Minimum Rating',
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Maximum Rating',
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 30,
+              mainAxisSpacing: 40,
+              padding: const EdgeInsets.all(10),
+              children: [
+                _buildPhotoListItem(
+                  imageUrl:
+                      'https://res.cloudinary.com/dhcvdux8e/image/upload/v1710443971/movie%20catalog/bastar.jpg',
+                  description: 'Bastar',
+                  rating: 4.5,
+                ),
+                _buildPhotoListItem(
+                  imageUrl:
+                      'https://res.cloudinary.com/dhcvdux8e/image/upload/f_auto,q_auto/v1/movie%20catalog/teri%20batoon',
+                  description: 'Teri batoon',
+                  rating: 5,
+                ),
+                _buildPhotoListItem(
+                  imageUrl:
+                      'https://miro.medium.com/v2/resize:fit:1400/1*dp9v_9BHhTwq6DPsFy84aw.jpeg',
+                  description: 'Masaan',
+                  rating: 5,
+                ),
+                _buildPhotoListItem(
+                  imageUrl:
+                      'https://res.cloudinary.com/dhcvdux8e/image/upload/v1710443971/movie%20catalog/bastar.jpg',
+                  description: 'Bastar',
+                  rating: 4.5,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPhotoListItem({
+    required String imageUrl,
+    required String description,
+    double? rating,
+  }) {
+    return Container(
+      height: 200,
+      width: 200,
+      child: Card(
+        color: const Color.fromARGB(255, 0, 0, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(
+              imageUrl,
+              height: 110,
+              width: 150,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              description,
+              style: const TextStyle(color: Colors.white),
+            ),
+            if (rating != null)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.star, color: Colors.yellow, size: 20),
+                  const SizedBox(width: 4),
+                  Text(
+                    rating.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+          ],
         ),
       ),
     );
